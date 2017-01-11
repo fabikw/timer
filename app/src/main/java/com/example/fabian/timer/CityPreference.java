cf8f006cee8182bd072273b4588bdb87cbcc507b
Fabian Kozynski <fabikw@gmail.com>
2017-01-11 19:24:35 -0300

    First commit

---
+++ app/src/main/java/com/example/fabian/timer/CityPreference.java
@@ -0,0 +1,26 @@
+package com.example.fabian.timer;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Fabian on 11-Jan-17.
+ */
+
+public class CityPreference {
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity){
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    // If the user has not chosen a city yet, return
+    // Sydney as the default city
+    String getCity(){
+        return prefs.getString("city", "Cambridge, US");
+    }
+
+    void setCity(String city){
+        prefs.edit().putString("city", city).commit();
+    }
+}
