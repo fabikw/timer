41d9d670a9704b7ba79a552d10833d385541a721
fabikw <fabikw@gmail.com>
2017-01-25 13:33:16 -0500

    Improved yahoo weather requests.
    Sometimes request won't work. Maybe store last request as a property.

--- app/src/main/java/com/example/fabian/timer/RemoteFetch.java
+++ app/src/main/java/com/example/fabian/timer/RemoteFetch.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by Fabian on 11-Jan-17.
@@ -19,19 +21,28 @@ public class RemoteFetch {
     private static final String YAHOO_API =
             "https://query.yahooapis.com/v1/public/yql?q=%s&format=json";
 
-    public static final String query = "select * where woeid in (select woeid from geo.places(1) where text = \"%s\")";
+    public static final String query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text = \"%s\")";
 
     public static JSONObject getJSON(Context context, String city){
         try {
-            String q = String.format(query,city);
+            String q = URLEncoder.encode(String.format(query,city),"UTF-8");
+            q = q.replace("+","%20");
             URL url = new URL(String.format(YAHOO_API, q));
-            URLConnection connection =
-                    (URLConnection)url.openConnection();
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
 
-            //connection.addRequestProperty("x-api-key", context.getString(R.string.open_weather_maps_app_id));
+            connection.addRequestProperty("x-api-key", context.getString(R.string.open_weather_maps_app_id));
+            int code = connection.getResponseCode();
+            // This value will be 404 if the request was not
+            // successful
+            if(code != 200){
+                return null;
+            }
+
+            InputStream i = connection.getInputStream();
 
             BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
+                    new InputStreamReader(i));
 
             StringBuffer json = new StringBuffer(1024);
             String tmp="";
@@ -41,11 +52,6 @@ public class RemoteFetch {
 
             JSONObject data = new JSONObject(json.toString());
 
-            // This value will be 404 if the request was not
-            // successful
-            if(data.getInt("cod") != 200){
-                return null;
-            }
 
             return data;
         }catch(Exception e){
